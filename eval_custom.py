9087641c9b0bb4428860264ffb6143b18aaa350b
Wei-Ming Lin <c7934597@gmail.com>
2021-01-09 15:39:36 +0800

    update

--- eval_custom.py
+++ eval_custom.py
@@ -44,8 +44,8 @@ class Evaluation(object):
 
         weight = os.path.join(weight_path)
         chkpt = torch.load(weight, map_location=self.__device)
-        self.__model.load_state_dict(chkpt["model"])
 #         self.__model.load_state_dict(chkpt["model"])
+        self.__model.load_state_dict(chkpt)
         print("loading weight file is done")
         del chkpt
 
@@ -96,7 +96,13 @@ class Evaluation(object):
                     path = os.path.join(
                         cfg.PROJECT_PATH, "detection_result/{}".format(v)
                     )
-
+                    
+                    dir_path = os.path.join(
+                        cfg.PROJECT_PATH, "detection_result/"
+                    )
+                    if not os.path.exists(dir_path):
+                        os.mkdir(dir_path)
+                        
                     cv2.imwrite(path, img)
                     logger.info("saved images : {}".format(path))
 
